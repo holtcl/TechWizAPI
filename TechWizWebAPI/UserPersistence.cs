239185c32db364093860458cd5a5c9a814aa6fbc
Matthew Miracle <matthew.miracle09@gmail.com>
2019-12-03 22:55:04 -0500

    updated project to allow android connection

--- TechWizWebAPI/UserPersistence.cs
+++ TechWizWebAPI/UserPersistence.cs
@@ -16,7 +16,7 @@ namespace TechWizWebAPI
         public UserPersistence()
         {
             string myConnectionString;
-            myConnectionString = "server=127.0.0.1;uid=root;pwd=techwizard;database=techwizard";
+            myConnectionString = "server=127.0.0.1;uid=root;pwd=password;database=techwizard";
             try
             {
                 conn = new MySql.Data.MySqlClient.MySqlConnection();
